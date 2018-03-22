d5d1101404bcb0965827cc6d520a20a0fbc7943b
S08_ICC <S08_ICC@vbx-schools.brunette.brucity.be>
2018-03-22 18:40:55 +0100

    New entity Category

---
+++ src/main/java/be/iccbxl/pid/category/Category.java
@@ -0,0 +1,38 @@
+package be.iccbxl.pid.category;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+	@Id
+	private int id;
+	private String name;
+		
+	public Category() {
+	}
+
+	public Category(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
